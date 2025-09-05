03e05bfd62a8c412a8180f8c7cf0d93d5e356351
Alessandro-45 <posixgen@gmail.com>
2025-09-05 07:57:19 -0400

    Leccion completa de hoy!

--- main.py
+++ main.py
@@ -4,7 +4,17 @@ import objetos
 
 
 pg.init()
+def detectar_colision(obj1, obj2):
+    rect1 = pg.Rect(obj1.posx, obj1.posy, obj1.ancho, obj1.alto)
+    rect2 = pg.Rect(obj2.posx, obj2.posy, obj2.ancho, obj2.alto)
+    return rect1.colliderect(rect2)
 
+def colision_con_pared(objeto, lista_paredes):
+    rect_obj = pg.Rect(objeto.posx, objeto.posy, objeto.ancho, objeto.alto)
+    for pared in lista_paredes:
+        if rect_obj.colliderect(pared.rect):
+            return True
+    return False
 
 personaje = objetos.Personaje(constantes.image_personaje, 
                               constantes.ancho_personaje,
@@ -17,32 +27,43 @@ enemy = objetos.Enemy(constantes.image_enemy,
                       constantes.pos_ini_enemy_x,
                       constantes.pos_ini_enemy_y,
                       constantes.velocidad_player)
+color1, color2, color3 = 163, 73, 164
+wall1 = objetos.Wall(color1, color2, color3, 50, 30,
+                     constantes.Ancho_ventana - 400,
+                     15)
+wall2 = objetos.Wall(color1, color2, color3, 50, 50,
+                     constantes.Ancho_ventana - 400,
+                     15)
+
+walls = [wall1, wall2]
+
+
 clock = pg.time.Clock()
 
 
-
+finish = False
 run = True
 while run == True:
     clock.tick(60)
-    objetos.Ventana.fill((255,255,255))
+    
     for eventos in pg.event.get():
 
         if eventos.type == pg.QUIT:
             run = False
-    personaje.dibujar()
-    personaje.update()
-    enemy.dibujar()
-    enemy.move()
+    if not finish:
+        objetos.Ventana.fill((255, 255, 255))
+        personaje.dibujar()
+        personaje.update()
+        enemy.dibujar()
+        enemy.move()
+        wall1.draw_wall()
+        wall2.draw_wall()
 
-    pg.display.flip()
-    
+        if detectar_colision(personaje, enemy) or colision_con_pared(personaje, walls):
+            objetos.Ventana.fill((163, 73, 164))
+            objetos.Ventana.blit(constantes.image_loser, (0,0))
+            finish = True
 
-
-
-
-
-
-
-
+    pg.display.update()
 pg.quit()
 
--- objetos.py
+++ objetos.py
@@ -57,3 +57,27 @@ class Enemy(GameObject):
             
             if self.posx <= 3:
                 constantes.dir_enemy = 'right'
+
+class Wall(pg.sprite.Sprite):
+    def __init__(self, color_1, color_2, color_3, wall_x, wall_y, wall_width, wall_height):
+       pg.sprite.Sprite.__init__(self)
+       self.color_1 = color_1
+       self.color_2 = color_2
+       self.color_3 = color_3
+       self.width = wall_width
+       self.height = wall_height
+
+
+       # imagen de la pared – un rectángulo de tamaño y color deseado
+       self.image = pg.Surface([self.width, self.height])
+       self.image.fill((color_1, color_2, color_3))
+
+
+       # cada objeto debe almacenar la propiedad rect (rectángulo)
+       self.rect = self.image.get_rect()
+       self.rect.x = wall_x
+       self.rect.y = wall_y
+
+
+    def draw_wall(self):
+      pg.draw.rect(Ventana, (self.color_1, self.color_2, self.color_3), (self.rect.x, self.rect.y, self.width, self.height))
